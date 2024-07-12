38314d7bcbb1989f6de57d57608835d0bd0e2f83
Pablo Suárez Lapalma <84790504+PabloSuarezLapalma@users.noreply.github.com>
2024-07-11 23:12:54 -0300

    Agregado de los comentarios sobre las funciones del admin

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,8 +1,17 @@
+//Se utiliza una constante global par almacenar la URL base de la API, que se utilizará para realizar las solicitudes HTTP.
 const localhost = 'http://localhost:8080/api_movie_maven_war';
+
+// Este código JavaScript se ejecuta cuando el DOM (Document Object Model) 
+// ha sido completamente cargado. 
+// Hace una solicitud GET a un servidor para obtener una lista de películas, 
+// y luego crea y añade elementos de tabla para cada película al cuerpo de una tabla.
 
 document.addEventListener('DOMContentLoaded', function () {
     // Function to fetch movies and update the table
     function fetchMovies() {
+        // realizamos una peticion de tipo http a esta api para obtener todas las peliculas de la base:
+        // El método GET se usa para recuperar datos, y se especifica que el contenido será JSON.
+        // configuracion de options, es un get y no necesita body
         const xhr = new XMLHttpRequest();
         xhr.open('GET', `${localhost}/movies`, true);
         xhr.send();
@@ -19,10 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
                             return;
                         }
 
-                        // Clear existing rows
+                        // Limpiar la tabla antes de agregar las películas
                         tableBody.innerHTML = '';
 
-                        // Append new rows
+                        // Recorremos todas las peliculas y agregamos su contenido a la tabla por cada pelicula
                         movies.forEach(movie => {
                             const row = document.createElement('tr');
                             row.innerHTML = `
@@ -43,10 +52,12 @@ document.addEventListener('DOMContentLoaded', function () {
         };
     }
 
-    // Call fetchMovies to load movies when the page loads
+    // LLamado a la función fetchMovies para que se ejecute al cargar la página
     fetchMovies();
 
     const agregarPelicula = document.getElementById('addMovieButton');
+
+    //Esta función se ejecuta cuando se hace clic en el botón Agregar película, utilizando el método POST para agregar una nueva película en base a los datos ingresados en el form.
 
     agregarPelicula.onclick =  function () {
         event.preventDefault();
@@ -55,8 +66,10 @@ document.addEventListener('DOMContentLoaded', function () {
         const genero = document.getElementById('genero').value;
         const imagen = document.getElementById('imagen').value;
 
+        //Se crea una nueva instancia de XMLHttpRequest y se abre una conexión POST a la URL de la API de películas.
         const xhr = new XMLHttpRequest();
         xhr.open('POST', `${localhost}/movies`, true);
+        //Se especifica que el tipo de contenido será pasado en formato JSON
         xhr.setRequestHeader('Content-Type', 'application/json');
         try{
             xhr.send(JSON.stringify({
@@ -69,11 +82,14 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Error al agregar pelicula:', e);
         }
         xhr.onreadystatechange = function () {
+            //Si la solicitud se ha completado y la respuesta está lista, se verifica si el estado de la respuesta es 201 (creado) y se muestra un mensaje en la consola.
             if (xhr.readyState === 4) {
                 if (xhr.status === 201) {
                     console.log('Pelicula agregada:', xhr.responseText);
+                    // Se llama a la función fetchMovies para actualizar la tabla con la nueva película.
                     fetchMovies();
                 } else {
+                    //Si la respuesta no es 201, se muestra un mensaje de error en la consola.
                     console.error(`Error al agregar pelicula: ${xhr.status} ${xhr.statusText}`);
                 }
             }
